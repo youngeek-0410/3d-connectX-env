2fde8c6597578bb72ae0e0fbba95f7e140891d91
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-19 19:39:14 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+import os
+
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+
+def main():
+    with open('README.md', 'r') as fp:
+        readme = fp.read()
+
+    setuptools.setup(
+        name='3d-connect4-gym',
+        version='1.0.0',
+        description='3D Connect4 for OpenAI Gym.',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        url='https://github.com/youngeek-0410/3d-connect4-gym',
+        license='',
+        author='Ryusei Ito',
+        author_email='31807@toyota.kosen-ac.jp',
+        packages=['gym_3d_connect4'],
+        install_requires=read_requirements(),
+        python_requires='>=3.6, <3.9',
+    )
+
+
+main()
