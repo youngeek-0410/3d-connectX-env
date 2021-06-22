3395f0f4c6ad2af5ff3ebd69f6f672e828906ca0
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-23 01:40:06 +0900

    Fix

--- test/test.py
+++ test/test.py
@@ -31,34 +31,24 @@ class TestCombination(unittest.TestCase):
                 "num_win_seq": 2
             }
         ]
+        self.envs = []
+
+        for pattern in self.pattern:
+            env = AnyNumberInARow3dEnv(num_grid=pattern["num_grid"], num_win_seq=pattern["num_win_seq"])
+            env.reset()
+
+            env.utils.win_reward = 100
+            env.utils.draw_penalty = 50
+            env.utils.lose_penalty = 100
+            env.utils.could_locate_reward = 10
+            env.utils.couldnt_locate_penalty = 10
+            env.utils.time_penalty = 1
+            env.player = 1
+            self.envs.append(env)
 
     @classmethod
     def tearDownClass(cls):
         print("Your environment has passed the test!!!!")
-
-    def setUp(self):
-        for pattern in self.pattern:
-            self.answer_dict = pattern
-            self.env = AnyNumberInARow3dEnv(num_grid=pattern["num_grid"], num_win_seq=pattern["num_win_seq"])
-            self.env.reset()
-
-            self.env.utils.win_reward = 100
-            self.env.utils.draw_penalty = 50
-            self.env.utils.lose_penalty = 100
-            self.env.utils.could_locate_reward = 10
-            self.env.utils.couldnt_locate_penalty = 10
-            self.env.utils.time_penalty = 1
-            self.env.player = 1
-
-    def test_pattern(self):
-        for idx, action in enumerate(self.answer_dict["actions"]):
-            obs, reward, done, info = self.env.step(action)
-
-            self.assertEqual(self.answer_dict["rewards"][idx], reward)
-            self.assertEqual(self.answer_dict["dones"][idx], done)
-            self.assertEqual(self.answer_dict["players"][idx], info["turn"])
-            self.assertEqual(self.answer_dict["winners"][idx], info["winner"])
-            self.assertEqual(self.answer_dict["couldnt_locates"][idx], info["is_couldnt_locate"])
 
 
 if __name__ == '__main__':
