16d0bd787d9911f115279507f15e52f05d5caf3b
m-star18 <31807@toyota.kosen-ac.jp>
2021-06-23 01:29:02 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,7 +4,8 @@ from gym_3d_connect4.envs import AnyNumberInARow3dEnv
 
 
 class TestCombination(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.pattern = [
             {
                 "actions": [0, 0, 1, 1, 2, 2, 4, 4, 0, 0, 1, 1, 2, 2, 0, 3],
@@ -31,30 +32,32 @@ class TestCombination(unittest.TestCase):
             }
         ]
 
+    def setUp(self):
+        for pattern in self.pattern:
+            self.answer_dict = pattern
+            self.env = AnyNumberInARow3dEnv(num_grid=pattern["num_grid"], num_win_seq=pattern["num_win_seq"])
+            self.env.reset()
+
+            self.env.utils.win_reward = 100
+            self.env.utils.draw_penalty = 50
+            self.env.utils.lose_penalty = 100
+            self.env.utils.could_locate_reward = 10
+            self.env.utils.couldnt_locate_penalty = 10
+            self.env.utils.time_penalty = 1
+            self.env.player = 1
+
     def tearDown(self):
         print("Your environment has passed the test!!!!")
 
     def test_pattern(self):
-        for pattern in self.pattern:
-            env = AnyNumberInARow3dEnv(num_grid=pattern["num_grid"], num_win_seq=pattern["num_win_seq"])
-            env.reset()
+        for idx, action in enumerate(self.answer_dict["actions"]):
+            obs, reward, done, info = self.env.step(action)
 
-            env.utils.win_reward = 100
-            env.utils.draw_penalty = 50
-            env.utils.lose_penalty = 100
-            env.utils.could_locate_reward = 10
-            env.utils.couldnt_locate_penalty = 10
-            env.utils.time_penalty = 1
-            env.player = 1
-
-            for idx, action in enumerate(pattern["actions"]):
-                obs, reward, done, info = env.step(action)
-
-                self.assertEqual(pattern["rewards"][idx], reward)
-                self.assertEqual(pattern["dones"][idx], done)
-                self.assertEqual(pattern["players"][idx], info["turn"])
-                self.assertEqual(pattern["winners"][idx], info["winner"])
-                self.assertEqual(pattern["couldnt_locates"][idx], info["is_couldnt_locate"])
+            self.assertEqual(self.answer_dict["rewards"][idx], reward)
+            self.assertEqual(self.answer_dict["dones"][idx], done)
+            self.assertEqual(self.answer_dict["players"][idx], info["turn"])
+            self.assertEqual(self.answer_dict["winners"][idx], info["winner"])
+            self.assertEqual(self.answer_dict["couldnt_locates"][idx], info["is_couldnt_locate"])
 
 
 if __name__ == '__main__':
