e6ffb8c550827ebd48a1eb372b4446e48e9acd97
m-star18 <31807@toyota.kosen-ac.jp>
2021-07-05 18:22:25 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 import unittest
 
-from gym_3d_connectX.envs import AnyNumberInARow3dEnv
+import gym
+import gym_3d_connectX
 
 
 class TestCombination(unittest.TestCase):
@@ -17,34 +18,21 @@ class TestCombination(unittest.TestCase):
                 "winners": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
                 "couldnt_locates": [False, False, False, False, False, False, False, False, False, False, False, False,
                                     False, False, True, False],
-                "num_grid": 4,
-                "num_win_seq": 4
             },
-            {
-                "actions": [0, 0, 0, 0, 8, 1],
-                "rewards": [10, 10, 10, -10, 10, 100],
-                "dones": [False, False, False, False, False, True],
-                "players": [1, -1, 1, -1, -1, 1],
-                "winners": [0, 0, 0, 0, 0, 1],
-                "couldnt_locates": [False, False, False, True, False, False],
-                "num_grid": 3,
-                "num_win_seq": 2
-            }
         ]
         self.envs = []
 
-        for pattern in self.pattern:
-            env = AnyNumberInARow3dEnv(num_grid=pattern["num_grid"], num_win_seq=pattern["num_win_seq"])
-            env.reset()
+        env = gym.make('3d-connectX-v0')
+        env.reset()
 
-            env.utils.win_reward = 100
-            env.utils.draw_penalty = 50
-            env.utils.lose_penalty = 100
-            env.utils.could_locate_reward = 10
-            env.utils.couldnt_locate_penalty = 10
-            env.utils.time_penalty = 1
-            env.player = 1
-            self.envs.append(env)
+        env.utils.win_reward = 100
+        env.utils.draw_penalty = 50
+        env.utils.lose_penalty = 100
+        env.utils.could_locate_reward = 10
+        env.utils.couldnt_locate_penalty = 10
+        env.utils.time_penalty = 1
+        env.player = 1
+        self.envs.append(env)
 
     @classmethod
     def tearDownClass(cls):
